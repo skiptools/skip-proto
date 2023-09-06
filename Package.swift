662c1a636d2ec16a45c2a9f629b5a22e9de4d853
marcprux <mwp1@cornell.edu>
2023-09-06 09:12:38 -0400

    Modernize package

--- Package.swift
+++ Package.swift
@@ -7,28 +7,15 @@ let package = Package(
     platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
     .library(name: "SkipProto", targets: ["SkipProto"]),
-    .library(name: "SkipProtoKt", targets: ["SkipProtoKt"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.0.0"),
-        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.0.0"),
-        .package(url: "https://source.skip.tools/skip-lib.git", from: "0.0.0"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.0.0"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.6.39"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.1.9"),
     ],
     targets: [
-    .target(name: "SkipProto", plugins: [.plugin(name: "skippy", package: "skip")]),
-    .target(name: "SkipProtoKt", dependencies: [
-        "SkipProto",
-        .product(name: "SkipUnitKt", package: "skip-unit"),
-        .product(name: "SkipLibKt", package: "skip-lib"),
-        .product(name: "SkipFoundationKt", package: "skip-foundation"),
-    ], resources: [.process("Skip")], plugins: [.plugin(name: "transpile", package: "skip")]),
+    .target(name: "SkipProto", dependencies: [.product(name: "SkipFoundation", package: "skip-foundation", condition: .when(platforms: [.macOS]))], plugins: [.plugin(name: "skipstone", package: "skip")]),
     .testTarget(name: "SkipProtoTests", dependencies: [
         "SkipProto"
-    ], plugins: [.plugin(name: "skippy", package: "skip")]),
-    .testTarget(name: "SkipProtoKtTests", dependencies: [
-        "SkipProtoKt",
-        .product(name: "SkipUnit", package: "skip-unit"),
-    ], resources: [.process("Skip")], plugins: [.plugin(name: "transpile", package: "skip")]),
+    ], plugins: [.plugin(name: "skipstone", package: "skip")]),
     ]
 )
--- Sources/SkipProtoKt/SkipProtoBundle.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-public extension Bundle {
-    static let SkipProtoBundle = Bundle.module
-}
